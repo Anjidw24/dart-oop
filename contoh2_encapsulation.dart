b0998a561e22fd88f645f02a513b4210c6874b2a
Anjidw24 <anjidwahyudi@yahoo.co.id>
2024-03-19 08:32:35 +0700

    contoh2_encapsulation

---
+++ contoh2_encapsulation.dart
@@ -0,0 +1,17 @@
+class Vehicle {
+  String? _model;
+  int? _year;
+
+  String? get model => _model;
+  set model(String? model) => _model = model;
+  int? get year => _year;
+  set year(int? year) => _year = year;
+}
+
+void main(List<String> args) {
+  var vehicle = Vehicle();
+  vehicle.model = "Toyota";
+  vehicle.year = 2019;
+  print(vehicle.model);
+  print(vehicle.year);
+}
